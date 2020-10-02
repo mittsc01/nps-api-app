3963adb668317cefb0ae1167055ede9654aaeb21
Scott Mittman <sm2884@nau.edu>
2020-10-02 11:45:40 -0500

    Made requested changes

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function retrieveParkData(aUrl,maxResults,state){
-    
+"use strict";
+const apiKey='xrDIQ1JpFCf18oDxURf21t8t9qZsaodwZJRmJJEV';
+const searchUrl="https://developer.nps.gov/api/v1/parks?parkCode=&";
+const endUrl='api_key=xrDIQ1JpFCf18oDxURf21t8t9qZsaodwZJRmJJEV';
 
-    fetch(aUrl).then(
+function formatQueryParams(params){
+    const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${params[key]}`)
+    return queryItems.join('&');
+}
+
+function retrieveParkData(states,maxResults){
+    const params ={
+        api_key:apiKey,
+        stateCode: states.join(','),
+        limit: maxResults
+    }
+    //console.log(params)
+    const queryString=formatQueryParams(params)
+    const url=searchUrl+queryString
+
+    fetch(url).then(
         (response)=>{
             if (response.ok){
                 return response.json()
@@ -10,33 +28,36 @@ function retrieveParkData(aUrl,maxResults,state){
             }
         
     ).then(
-        data=>displayResults(data,maxResults,state)
+        response=>displayResults(response.data)
     )
     .catch(
         err=>$('#js-error-message').text(`Something went wrong: ${err.message}`)
     )
 }
 
-function displayResults(data,num,state){
+function displayResults(data){
+    $('#display-results').removeClass('hidden')
     $('#js-error-message').text('')
     $('#display-results').html('')
-    $('#display-results').html(buildTemplate(data,num,state))
+    $('#display-results').html(parkListTemplate(data))
 }
-function buildTemplate(data,num,state){
-    let bigString=`<h2>${state}</h2>`;
+
+
+function parkListTemplate(data){
+    let bigString='';
     
-    //TODO: finish buildTemplate
+    
     let listItems=''
-    for (let i=0;i<Math.min(num,data['data'].length);i++){
-        listItems+=`<li>${data['data'][i].fullName}
+    for (let i=0;i<data.length;i++){
+        listItems+=`<li>${data[i].fullName}
             <ul>
-                <li>${data['data'][i].description}</li>
-                <li>Website:<a href=${data['data'][i].url}>${data['data'][i].url}<a></li>
+                <li>${data[i].description}</li>
+                <li>Website:<a href=${data[i].url}>${data[i].url}<a></li>
                 <li>Park address:
                     <br>
-                    ${data['data'][i].addresses[0].line1}
+                    ${data[i].addresses[0].line1}
                     <br>
-                    ${data['data'][i].addresses[0].city}, ${data['data'][i].addresses[0].stateCode} ${data['data'][i].addresses[0].postalCode}
+                    ${data[i].addresses[0].city}, ${data[i].addresses[0].stateCode} ${data[i].addresses[0].postalCode}
                 </li>
             </ul>
         
@@ -47,23 +68,30 @@ function buildTemplate(data,num,state){
     return bigString
 }
 
-function constructUrl(state) {
-    const baseUrl="https://developer.nps.gov/api/v1/parks?parkCode=&";
-    const endUrl='api_key=xrDIQ1JpFCf18oDxURf21t8t9qZsaodwZJRmJJEV';
-    
-    const stateUrl=`stateCode=${state.toLowerCase()}&`;
-    
-    
-    return baseUrl+stateUrl+endUrl;
-}
+
 
 
 function watchForm(){
     $('form').on('submit',(e)=>{
         e.preventDefault()
-        const state=e.target.states.value;
+        
+        let choices=document.getElementById('states')
+        let selections=[];
+        
+        for (let  i=0;i<choices.length;i++){
+            if (choices[i].selected){
+                
+                selections.push(choices[i].value)
+  
+            }
+        }
+        //const state=e.target.states.value;
+        //console.log(state)
+        
+        
         const maxResults=e.target['max-results'].value
-        retrieveParkData(constructUrl(state),maxResults,state);
+        retrieveParkData(selections,maxResults);
+        
         
         
 
